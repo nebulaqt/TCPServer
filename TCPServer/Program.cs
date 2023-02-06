93ce27e8f72e95d5706ee1180b20861969f7230b
nebula <jldacombe@gmail.com>
2023-02-06 15:40:34 +1300

    update

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -10,19 +10,21 @@ namespace TCPServer
     {
         static void Main(string[] args)
         {
-            // Create a TCP listener object and start it
+            // Create a TCP listener object with IP address set to any available IP and port 1252
             TcpListener listener = new TcpListener(IPAddress.Any, 1252);
+            // Start the listener to listen for incoming connections
             listener.Start();
 
-            // Continuously listen for incoming connections
+            // Continuously listen for incoming connections from clients
             while (true)
             {
-                // Wait for a bit to prevent the CPU from going crazy
+                // Wait for 250 milliseconds to prevent the CPU from overloading
                 Thread.Sleep(250);
-                // Accept a new client connection
+
+                // Accept incoming connection from a client
                 TcpClient client = listener.AcceptTcpClient();
 
-                // Create a new thread to handle the connection
+                // Create a new thread to handle the client's connection
                 Thread thread = new Thread(new ParameterizedThreadStart(HandleClient));
                 thread.Start(client);
             }
@@ -30,41 +32,55 @@ namespace TCPServer
 
         static void HandleClient(object obj)
         {
-            // Get the client object from the parameter
+            // Convert the object to TcpClient type
             TcpClient client = (TcpClient)obj;
 
-            // Get the client's network stream
-            NetworkStream stream = client.GetStream();
-
-            // Continuously read from the stream
-            while (true)
+            try
             {
-                // Wait for a bit to prevent the CPU from going crazy
-                Thread.Sleep(250);
-                
-                // Read data from the stream
-                byte[] buffer = new byte[1024];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                // Get the network stream from the client
+                NetworkStream stream = client.GetStream();
 
-                // Check if the message is not empty
-                if (!string.IsNullOrEmpty(message))
+                // Continuously read data from the stream
+                while (true)
                 {
-                    // Log the message
-                    Console.WriteLine("Received message: " + message);
-                }
+                    // Wait for 250 milliseconds to prevent the CPU from overloading
+                    Thread.Sleep(250);
 
-                // Check if the message is the "keep alive" message
-                if (message.Trim() == "keep alive")
-                {
-                    // Send a response back to the client
-                    byte[] response = Encoding.ASCII.GetBytes("ack");
-                    stream.Write(response, 0, response.Length);
+                    // Read data from the stream into a buffer
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    // Convert the data into a string
+                    string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                    // Check if the message is not empty
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        // Write the message to the console
+                        Console.WriteLine("Received message: " + message);
+                    }
+
+                    // Check if the message is "keep alive"
+                    if (message.Trim() == "keep alive")
+                    {
+                        // Send the "ack" response back to the client
+                        byte[] response = Encoding.ASCII.GetBytes("ack");
+                        stream.Write(response, 0, response.Length);
+                    }
+                    else
+                    {
+                        // Handle other messages as needed
+                    }
                 }
-                else
-                {
-                    // Handle other messages as needed
-                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occurred while handling client: " + ex.Message);
+            }
+            finally
+            {
+                // Close the client connection
+                client.Close();
             }
         }
     }
