96f08bf5df6f9c8af6b4206730d283116231c443
nebula <jlevidacombe@gmail.com>
2023-06-16 01:43:00 +1200

    update

---
+++ TCPServer/TCPServer/TcpServerFrm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TCPServer
+{
+    partial class TcpServerFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.statusLb = new System.Windows.Forms.Label();
+            this.logRtb = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(12, 12);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(92, 33);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(480, 12);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(92, 33);
+            this.stopBtn.TabIndex = 1;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // statusLb
+            // 
+            this.statusLb.Location = new System.Drawing.Point(12, 48);
+            this.statusLb.Name = "statusLb";
+            this.statusLb.Size = new System.Drawing.Size(560, 31);
+            this.statusLb.TabIndex = 2;
+            this.statusLb.Text = "Status: IP:Port";
+            this.statusLb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // logRtb
+            // 
+            this.logRtb.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.logRtb.DetectUrls = false;
+            this.logRtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logRtb.Location = new System.Drawing.Point(12, 82);
+            this.logRtb.Name = "logRtb";
+            this.logRtb.ReadOnly = true;
+            this.logRtb.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.logRtb.ShortcutsEnabled = false;
+            this.logRtb.Size = new System.Drawing.Size(560, 167);
+            this.logRtb.TabIndex = 3;
+            this.logRtb.TabStop = false;
+            this.logRtb.Text = "";
+            // 
+            // TcpServerFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 261);
+            this.Controls.Add(this.logRtb);
+            this.Controls.Add(this.statusLb);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Name = "TcpServerFrm";
+            this.Text = "TCPServer";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.RichTextBox logRtb;
+
+        private System.Windows.Forms.Label statusLb;
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+
+        #endregion
+    }
+}
---
+++ TCPServer/TCPServer/TcpServerFrm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TCPServer
+{
+    public partial class TcpServerFrm : Form
+    {
+        private Thread _acceptClientsThread; // Thread for accepting client connections
+        private CancellationTokenSource _cancellationTokenSource; // Source for canceling asynchronous operations
+        private bool _isServerRunning; // Flag to track the server running state
+        private TcpListener _listener; // TCP listener for incoming connections
+
+        public TcpServerFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            try
+            {
+                // Enable TCP keep-alive on the client socket
+                client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+
+                var stream = client.GetStream(); // Get the network stream for reading and writing data
+                var buffer = new byte[1024]; // Buffer to hold the received data
+
+                while (!_cancellationTokenSource.Token.IsCancellationRequested)
+                {
+                    if (stream.DataAvailable) // Check if there is data available to read from the stream
+                    {
+                        var bytesRead = stream.Read(buffer, 0, buffer.Length); // Read data from the stream into the buffer
+                        var message = Encoding.ASCII.GetString(buffer, 0, bytesRead); // Convert the received bytes to a string
+                        var clientEndPoint = client.Client.RemoteEndPoint as IPEndPoint; // Get the client's IP endpoint
+
+                        if (!string.IsNullOrEmpty(message)) // Check if the received message is not empty
+                        {
+                            var logMessage = $"Received message from {clientEndPoint}: {message}\n"; // Create a log message
+                            AppendToLog(logMessage); // Append the log message to the logRichTextBox
+                        }
+
+                        if (message.Trim() == "keep alive") // Check if the received message is "keep alive"
+                        {
+                            var response = Encoding.ASCII.GetBytes("ack"); // Convert the response string to bytes
+                            stream.Write(response, 0, response.Length); // Write the response to the stream
+                        }
+                    }
+
+                    // Introduce a short delay to reduce CPU usage
+                    Thread.Sleep(10);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(@"Error occurred while handling client: " + ex.Message);
+            }
+            finally
+            {
+                client.Close(); // Close the client connection
+            }
+        }
+
+        private void AcceptClients()
+        {
+            while (_isServerRunning)
+            {
+                try
+                {
+                    var client = _listener.AcceptTcpClient(); // Accept an incoming TCP client connection
+                    ThreadPool.QueueUserWorkItem(state => HandleClient(client)); // Queue the client for processing on a thread pool thread
+                }
+                catch (ObjectDisposedException)
+                {
+                    // Handle ObjectDisposedException if the server is stopped
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(@"Error occurred while accepting client: " + ex.Message);
+                }
+            }
+        }
+
+        private void AppendToLog(string logMessage)
+        {
+            if (logRtb.InvokeRequired) // Check if invoking is required to access the UI control from a different thread
+            {
+                logRtb.BeginInvoke((MethodInvoker)(() =>
+                {
+                    logRtb.AppendText(logMessage); // Append the log message to the logRichTextBox
+                    logRtb.ScrollToCaret(); // Scroll to the end of the logRichTextBox
+                }));
+            }
+            else
+            {
+                logRtb.AppendText(logMessage); // Append the log message to the logRichTextBox
+                logRtb.ScrollToCaret(); // Scroll to the end of the logRichTextBox
+            }
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (_isServerRunning)
+                return;
+
+            _listener = new TcpListener(IPAddress.Any, 1252); // Create a TCP listener on all available network interfaces and port 1252
+            _listener.Start(); // Start listening for incoming connections
+            _isServerRunning = true; // Set the server running flag to true
+            startBtn.Enabled = false; // Disable the Start button
+            stopBtn.Enabled = true; // Enable the Stop button
+
+            _cancellationTokenSource = new CancellationTokenSource(); // Create a new cancellation token source
+            // Update label with IP address and port
+            statusLb.Text = @"Status: Running " + _listener.LocalEndpoint; // Set the status label text to the server's local endpoint
+
+            _acceptClientsThread = new Thread(AcceptClients); // Create a new thread for accepting clients
+            _acceptClientsThread.Start(); // Start the thread for accepting clients
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            if (!_isServerRunning)
+                return;
+
+            // Update label with IP address and port
+            statusLb.Text = @"Status: Stopped"; // Set the status label text to "Stopped"
+            _listener.Stop(); // Stop the TCP listener
+            _isServerRunning = false; // Set the server running flag to false
+            _cancellationTokenSource?.Cancel(); // Cancel any pending asynchronous operations
+            _cancellationTokenSource?.Dispose(); // Dispose the cancellation token source
+            _cancellationTokenSource = null; // Set the cancellation token source to null
+            stopBtn.Enabled = false; // Disable the Stop button
+            startBtn.Enabled = true; // Enable the Start button
+        }
+    }
+}
